b4b353395548850ff8c8279c35082456942d8bbe
MINGW64 <Adrijesuslerma@outlook.com>
2024-04-17 15:55:49 -0700

    avances2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
 import "./App.css";
+import { Route, Routes, Link, NavLink } from "react-router-dom";
+import Formulario from "./Formulario";
+
+//Componente con los elementos del formulario para iniciar sesion
+<Formulario />;
+
+//Componente con los elementos del menu de peliculas
+const Inicio = () => <h1>Catalogo de peliculas</h1>;
+
+//Componente con los elementos de la page para ver las peliculas ya vistas
+const Vistas = () => <h1>Peliculas vistas</h1>;
+
+//Componente con los elementos de la page para ver tus peliculas favoritas
+const Favoritas = () => <h1>Peliculas Favoritas</h1>;
 
 function App() {
   return (
     <>
-      <div className="App"></div>
+      <nav className="Navegador text-decoration-none w-full h-20 bg-gradient-to-r from-orange-500 to-orange-800  ">
+        <ul
+          id="Opciones"
+          className="p-2 w-full h-full text-2xl flex justify-between items-center"
+        >
+          <li>
+            <NavLink to="/">iniciar sesion</NavLink>
+          </li>
+
+          <li>
+            <Link to="/Catalogo">Peliculas</Link>
+          </li>
+
+          <li>
+            <Link to="/PeliculasVistas">Peliculas Vistas</Link>
+          </li>
+
+          <li>
+            <Link to="/PeliculasFavoritas"> Favoritas </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Formulario />}></Route>
+          <Route path="/Catalogo" element={<Inicio />}></Route>
+          <Route path="/PeliculasVistas" element={<Vistas />}></Route>
+          <Route path="/PeliculasFavoritas" element={<Favoritas />}></Route>
+        </Routes>
+      </div>
     </>
   );
 }
--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,3 +1,62 @@
-<form className="bg-red-500">
-  <h1>Hola</h1>
-</form>;
+import { useState } from "react";
+
+const Formulario = () => {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      userEmail === "adrianlerma@gmail.com" &&
+      userPassword === "pipipopo123"
+    ) {
+      // Credenciales correctas, redirigir a otra página
+    } else {
+      // Credenciales incorrectas, mostrar mensaje de error
+      setError("El correo o la contraseña son incorrectos, intente de nuevo");
+    }
+  };
+
+  return (
+    <div
+      id="formulario"
+      className="flex flex-col h-[872px] items-center justify-center"
+    >
+      <h1 className="text-white text-5xl m-8">Iniciar sesión</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        <label className="text-white text-xl px-2">Correo del usuario</label>
+        <input
+          className="text-black px-2 mb-3 rounded-lg w-80 h-8 mx-3"
+          type="email"
+          name="userEmail"
+          id="userEmail"
+          value={userEmail}
+          onChange={(e) => setUserEmail(e.target.value)}
+          placeholder="Ingrese su correo"
+          autoComplete="off"
+        />
+        <label className="text-white text-xl px-2">Contraseña</label>
+        <input
+          className="text-black px-2 mb-4 rounded-lg w-80 h-8 mx-3"
+          type="password"
+          name="userPassword"
+          id="userPassword"
+          value={userPassword}
+          onChange={(e) => setUserPassword(e.target.value)}
+          placeholder="Ingrese su contraseña"
+          autoComplete="off"
+        />
+        <button
+          type="submit"
+          className="bg-orange-500 rounded-lg text-black mx-3 h-10 text-xl hover:scale-105 transition-transform hover:bg-orange-700 hover:text-white hover:duration-300"
+        >
+          Ingresar
+        </button>
+        {error && <p className="text-red-500 ">{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
